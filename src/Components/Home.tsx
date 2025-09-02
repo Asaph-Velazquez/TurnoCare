37ff8243f7084e8e2f275a8862e5f973a9cbb437
Asaph Velazquez <saulvelpa120@gmail.com>
2025-09-01 18:07:21 -0600

    Planteamiento incial de la pagina home

---
+++ src/Components/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2>Página Principal</h2>
+      <p>Bienvenido a TurnoCare, tu app de citas médicas.</p>
+      <h1> ESTAMOS TRABAJANDO EN ESTO</h1>
+    </div>
+  );
+}
+export default Home;
