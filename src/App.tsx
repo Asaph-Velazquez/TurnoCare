592eadab87795db30bfb2e4752e49e7a4b8c08c9
Asaph Velazquez <saulvelpa120@gmail.com>
2025-10-08 12:40:07 -0600

    Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-import UserHome from './Components/UserHome';
-import Nav from './Components/UserNav';
+import UserNav from './Components/UserNav';
 import Login from './Components/Login';
 
 function App() {
   return (
     <div>
-      <Nav />
+      <UserNav />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<UserHome />} />
-        <Route path="/nav" element={<div>Nav Component</div>} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </div>
   );
